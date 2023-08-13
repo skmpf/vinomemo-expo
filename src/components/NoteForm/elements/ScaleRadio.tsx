7bdf6c4436394d05521ee825ef17ad0666c312ca
Sebastien Kempf <skempf@deezer.com>
2023-08-13 11:24:30 +0200

    feat: create scale radio input

---
+++ src/components/NoteForm/elements/ScaleRadio.tsx
@@ -0,0 +1,76 @@
+import { useState } from "react";
+import { Pressable, View } from "react-native";
+import { Text, useTheme } from "@rneui/themed";
+import { useFormikContext } from "formik";
+import { INote } from "@/modules/note";
+
+type ScaleRadioProps = {
+  label: string;
+  name: string;
+  options: string[];
+  value: string;
+};
+
+export const ScaleRadio: React.FC<ScaleRadioProps> = ({
+  label,
+  name,
+  options,
+  value,
+}) => {
+  const { theme } = useTheme();
+  const { setFieldValue } = useFormikContext<INote>();
+  const [selected, setSelected] = useState(value);
+  const maxHeight = 40;
+
+  const handleSelect = (option: string) => {
+    setSelected(option);
+    setFieldValue(name, option);
+  };
+
+  return (
+    <View style={{ marginBottom: 20 }}>
+      <Text style={{ color: theme.colors.primary, marginBottom: 10 }}>
+        {label}
+      </Text>
+      <View
+        style={{
+          flexDirection: "row",
+        }}
+      >
+        {options.map((option, index) => (
+          <View
+            key={option}
+            style={{
+              flex: 1,
+              justifyContent: "flex-end",
+            }}
+          >
+            <Pressable
+              onPress={() => handleSelect(option)}
+              style={{
+                height: (maxHeight * (index + 1)) / options.length,
+                maxHeight: maxHeight,
+                borderRadius: 10,
+                marginHorizontal: 5,
+                backgroundColor:
+                  selected === option
+                    ? theme.colors.primary
+                    : theme.colors.grey5,
+              }}
+            />
+            <Text
+              style={{
+                fontSize: 12,
+                color: theme.colors.primary,
+                textTransform: "uppercase",
+                textAlign: "center",
+              }}
+            >
+              {option}
+            </Text>
+          </View>
+        ))}
+      </View>
+    </View>
+  );
+};
